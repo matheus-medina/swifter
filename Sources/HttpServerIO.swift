34014b2b4e56ea722f1ad8f5514c827a29d487a0
Gustavo Tiago <gu.tiagosilva@gmail.com>
2017-11-23 20:05:25 -0200

    Update HttpServerIO.swift

--- Sources/HttpServerIO.swift
+++ Sources/HttpServerIO.swift
@@ -13,62 +13,62 @@ public protocol HttpServerIODelegate: class {
 }
 
 public class HttpServerIO {
-
+    
     public weak var delegate : HttpServerIODelegate?
-
+    
     private var socket = Socket(socketFileDescriptor: -1)
     private var sockets = Set<Socket>()
-
+    
     public enum HttpServerIOState: Int32 {
         case starting
         case running
         case stopping
         case stopped
     }
-
+    
     private var stateValue: Int32 = HttpServerIOState.stopped.rawValue
-
+    
     public private(set) var state: HttpServerIOState {
         get {
             return HttpServerIOState(rawValue: stateValue)!
         }
         set(state) {
             #if !os(Linux)
-            OSAtomicCompareAndSwapInt(self.state.rawValue, state.rawValue, &stateValue)
+                OSAtomicCompareAndSwapInt(self.state.rawValue, state.rawValue, &stateValue)
             #else
-            //TODO - hehe :)
-            self.stateValue = state.rawValue
+                //TODO - hehe :)
+                self.stateValue = state.rawValue
             #endif
         }
     }
-
+    
     public var operating: Bool { get { return self.state == .running } }
-
+    
     /// String representation of the IPv4 address to receive requests from.
     /// It's only used when the server is started with `forceIPv4` option set to true.
     /// Otherwise, `listenAddressIPv6` will be used.
     public var listenAddressIPv4: String?
-
+    
     /// String representation of the IPv6 address to receive requests from.
     /// It's only used when the server is started with `forceIPv4` option set to false.
     /// Otherwise, `listenAddressIPv4` will be used.
     public var listenAddressIPv6: String?
-
+    
     private let queue = DispatchQueue(label: "swifter.httpserverio.clientsockets")
     private let socketQueue = DispatchQueue(label: "swifter.httpserverio.serversockets", qos: .userInitiated, attributes: .concurrent)
     
     public func port() throws -> Int {
         return Int(try socket.port())
     }
-
+    
     public func isIPv4() throws -> Bool {
         return try socket.isIPv4()
     }
-
+    
     deinit {
         stop()
     }
-
+    
     @available(macOS 10.10, *)
     public func start(_ port: in_port_t = 8080, forceIPv4: Bool = false, priority: DispatchQoS.QoSClass = DispatchQoS.QoSClass.userInitiated) throws {
         guard !self.operating else { return }
@@ -78,38 +78,39 @@ public class HttpServerIO {
         self.socket = try Socket.tcpSocketForListen(port, forceIPv4, SOMAXCONN, address)
         self.state = .running
         self.socketQueue.async { [weak self] in
-            guard let `self` = self else { return }
-            guard self.operating else { return }
-            while let socket = try? self.socket.acceptClientSocket() {
-                self.socketQueue.async { [weak self] in
-                    guard let `self` = self else { return }
-                    guard self.operating else { return }
-                    self.queue.async {
-                        self.sockets.insert(socket)
+            guard let strongSelf = self else { return }
+            guard strongSelf.operating else { return }
+            while let socket = try? strongSelf.socket.acceptClientSocket() {
+                strongSelf.socketQueue.async { [weak self] in
+                    guard let strongSelf = self else { return }
+                    guard strongSelf.operating else { return }
+                    strongSelf.queue.async { [weak self] in
+                        guard let strongSelf = self else { return }
+                        strongSelf.sockets.insert(socket)
                     }
-                    self.handleConnection(socket)
-                    self.queue.async {
-                        self.sockets.remove(socket)
+                    strongSelf.handleConnection(socket)
+                    strongSelf.queue.async { [weak self] in
+                        guard let strongSelf = self else { return }
+                        strongSelf.sockets.remove(socket)
                     }
                 }
             }
-            self.stop()
+            strongSelf.stop()
             DispatchQueue.main.async {
-                let isActive = UIApplication.shared.applicationState == .active
-                if isActive {
-                    self.socketQueue.async {
+                if UIApplication.shared.applicationState == .active {
+                    strongSelf.socketQueue.async { [weak self] in
+                        guard let strongSelf = self else { return }
                         do {
-                            try self.start(port)
+                            try strongSelf.start(port)
                         } catch {
                             print("Server start error: \(error)")
                         }
                     }
                 }
             }
-         
         }
     }
-
+    
     public func stop() {
         guard self.operating else { return }
         self.state = .stopping
@@ -117,17 +118,17 @@ public class HttpServerIO {
         for socket in self.sockets {
             socket.close()
         }
-        self.queue.sync {
-            self.sockets.removeAll(keepingCapacity: true)
+        self.queue.sync { [weak self] in
+            self?.sockets.removeAll(keepingCapacity: true)
         }
         socket.close()
         self.state = .stopped
     }
-
+    
     public func dispatch(_ request: HttpRequest) -> ([String: String], (HttpRequest) -> HttpResponse) {
         return ([:], { _ in HttpResponse.notFound })
     }
-
+    
     private func handleConnection(_ socket: Socket) {
         let parser = HttpParser()
         while self.operating, let request = try? parser.readHttpRequest(socket) {
@@ -154,58 +155,58 @@ public class HttpServerIO {
         }
         socket.close()
     }
-
+    
     private struct InnerWriteContext: HttpResponseBodyWriter {
         
         let socket: Socket
-
+        
         func write(_ file: String.File) throws {
             try socket.writeFile(file)
         }
-
+        
         func write(_ data: [UInt8]) throws {
             try write(ArraySlice(data))
         }
-
+        
         func write(_ data: ArraySlice<UInt8>) throws {
             try socket.writeUInt8(data)
         }
-
+        
         func write(_ data: NSData) throws {
             try socket.writeData(data)
         }
-
+        
         func write(_ data: Data) throws {
             try socket.writeData(data)
         }
     }
-
+    
     private func respond(_ socket: Socket, response: HttpResponse, keepAlive: Bool) throws -> Bool {
         guard self.operating else { return false }
-
+        
         try socket.writeUTF8("HTTP/1.1 \(response.statusCode()) \(response.reasonPhrase())\r\n")
-
+        
         let content = response.content()
-
+        
         if content.length >= 0 {
             try socket.writeUTF8("Content-Length: \(content.length)\r\n")
         }
-
+        
         if keepAlive && content.length != -1 {
             try socket.writeUTF8("Connection: keep-alive\r\n")
         }
-
+        
         for (name, value) in response.headers() {
             try socket.writeUTF8("\(name): \(value)\r\n")
         }
-
+        
         try socket.writeUTF8("\r\n")
-
+        
         if let writeClosure = content.write {
             let context = InnerWriteContext(socket: socket)
             try writeClosure(context)
         }
-
+        
         return keepAlive && content.length != -1;
     }
 }
